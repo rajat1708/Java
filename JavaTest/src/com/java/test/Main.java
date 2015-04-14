f3c3bb04400726e9c4b6063b829b809a036b7120
Rajat <kasliwalrajat1708@gmail.com>
2015-04-14 17:58:51 +0530

    Commit

---
+++ JavaTest/src/com/java/test/Main.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package com.java.test;
+
+/**
+ * @author rajat
+ *
+ */
+public class Main {
+
+	/**
+	 * 
+	 */
+	public Main() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello");
+
+	}
+
+}
